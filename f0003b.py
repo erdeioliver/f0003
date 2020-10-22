1e30a01b8b081332f242c6ef30256b688ceeb9be
erdeiolver <71698306+erdeiolver@users.noreply.github.com>
2020-10-22 22:13:26 +0200

    Add files via upload

---
+++ f0003b.py
@@ -0,0 +1,6 @@
+vezetéknév = input('Szia, mi a vezeték neved? ')
+Keresztnév = input('És mi a kereszt neved? ')
+Születes = input('Mikor születél? ')
+csilag = input('Mi a csillagjegyed? ')
+print('A te neved ', vezetéknév, ' ', Keresztnév, '.', sep='')
+print('Valamint te ', Születes, ' születél és a csillagjegyed pedig ez ', csilag, '.', sep='')
